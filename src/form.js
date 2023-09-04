794c25476c59d9247ba7a0bdcdb2a3a4de9d51e7
Caroletisan <carolsantos_leticia@hotmail.com>
2023-09-04 19:54:27 -0300

    base do projeto (html/css/js) com uso do vite

---
+++ src/form.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    try{
+        StartLoading()
+        // pegar o form
+        // loading
+    } catch(error) {
+
+    }
+})
